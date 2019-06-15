7bc559ab4a6e734efd987ffaba1f971ecb8c8b02
Engineering Tech <51522195+firebolt-minion@users.noreply.github.com>
2019-06-15 13:52:43 -0700

    Update power_module.ino
    Voltage sensor code with scaled value.

--- power_module/power_module.ino
+++ power_module/power_module.ino
@@ -1,5 +1,5 @@
 
-int offset =20;// set the correction offset value
+int offset =3;// set the correction offset value
 const int Vsensor_pin=34;
 
 void setup() {
@@ -9,16 +9,16 @@ void setup() {
 void loop() {
   
   int volt = analogRead(Vsensor_pin);// read the input
-
+  double voltage_scal = 0.0036*volt+2.1748;
   //double voltage = Vsensor_pin/4095;
   
-  //double voltage = map(volt,0,1023, 0, 2500) + offset;// map 0-1023 to 0-2500 and add correction offset
+  //double voltage_out = map(volt,0,4095, 0, 24); //- offset;// map 0-1023 to 0-2500 and add correction offset
   //voltage /=100;// divide by 100 to get the decimal values
   //Serial.print("Voltage: ");
   //Serial.print(voltage);//print the voltge
   //Serial.println("V");
 
-  Serial.println(volt);
+  Serial.println(voltage_scal);
   
 
 
