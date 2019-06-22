5b226fa287044ebb221c830752e8bb1dc77e61c8
Engineering Tech <51522195+firebolt-minion@users.noreply.github.com>
2019-06-22 10:03:16 -0700

    Update power_module.ino

--- power_module.ino
+++ power_module.ino
@@ -1,4 +1,4 @@
-
+#include "math.h"
 int offset =3;// set the correction offset value
 const int Vsensor_pin=34;
 
@@ -9,20 +9,27 @@ void setup() {
 void loop() {
   
   int volt = analogRead(Vsensor_pin);// read the input
-  double voltage_scal = 0.0036*volt+2.1748;
-  //double voltage = Vsensor_pin/4095;
-  
-  //double voltage_out = map(volt,0,4095, 0, 24); //- offset;// map 0-1023 to 0-2500 and add correction offset
-  //voltage /=100;// divide by 100 to get the decimal values
+if (volt < 2429){
+  double voltage_scal = 0.0043*volt+0.4894; //scaled voltage for 0-10.99v
   //Serial.print("Voltage: ");
-  //Serial.print(voltage);//print the voltge
-  //Serial.println("V");
-
   Serial.println(voltage_scal);
+}
+
+//2429 is the analog voltage at which we will switch over to equation two to display scaled voltage
+ else if (volt < 3293 && volt > 2429){
+  double voltage_scal = 0.0036*volt+2.1748; //scaled voltage for 11-13.99v
+  //Serial.print("Voltage: ");
+  Serial.println(voltage_scal);
+ }
+   //3293 is the analog voltage reading at which we will switch over to equation three to display scaled voltage
+ else {
+  double voltage_scal = 0.0023*volt+6.3361; // scaled voltage for 14-15v
+    //Serial.print("Voltage: ");
+  Serial.println(voltage_scal);
+ }
   
-
-
-delay(500);
+  
+  delay(1000);
   
   
 }
