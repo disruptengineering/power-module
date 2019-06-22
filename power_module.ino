c9e6be5a5ef8c9451b630cbfc56fc40cfc0a81cc
Isaac Jo <isaacjo91@gmail.com>
2019-06-22 11:02:20 -0700

    Code cleanup, wrapping voltage reading in function

--- power_module.ino
+++ power_module.ino
@@ -1,6 +1,10 @@
 #include "math.h"
-int offset =3;// set the correction offset value
+
+//PINS
+
+//Voltage sensor
 const int Vsensor_pin=34;
+//Current sensor
 
 void setup() {
   Serial.begin(115200);
@@ -8,28 +12,33 @@ void setup() {
 
 void loop() {
   
-  int volt = analogRead(Vsensor_pin);// read the input
-if (volt < 2429){
-  double voltage_scal = 0.0043*volt+0.4894; //scaled voltage for 0-10.99v
-  //Serial.print("Voltage: ");
-  Serial.println(voltage_scal);
-}
-
-//2429 is the analog voltage at which we will switch over to equation two to display scaled voltage
- else if (volt < 3293 && volt > 2429){
-  double voltage_scal = 0.0036*volt+2.1748; //scaled voltage for 11-13.99v
-  //Serial.print("Voltage: ");
-  Serial.println(voltage_scal);
- }
-   //3293 is the analog voltage reading at which we will switch over to equation three to display scaled voltage
- else {
-  double voltage_scal = 0.0023*volt+6.3361; // scaled voltage for 14-15v
-    //Serial.print("Voltage: ");
-  Serial.println(voltage_scal);
- }
-  
-  
+  voltage = getVoltage();
+  Serial.println(voltage);
   delay(1000);
   
+}
+
+//FUNCTIONS
+
+double getVoltage(){
+
+  int volt = analogRead(Vsensor_pin);// read the input
+
+  if (volt < 2429){
+    double voltage_scal = 0.0043*volt+0.4894; //scaled voltage for 0-10.99v
+    return voltage_scal;
+  }
+
+  //2429 is the analog voltage at which we will switch over to equation two to display scaled voltage
+  else if (volt < 3293 && volt > 2429){
+    double voltage_scal = 0.0036*volt+2.1748; //scaled voltage for 11-13.99v
+    return voltage_scal
+  }
+
+  //3293 is the analog voltage reading at which we will switch over to equation three to display scaled voltage
+  else {
+    double voltage_scal = 0.0023*volt+6.3361; // scaled voltage for 14-15v
+    return voltage_scal
+  }
   
 }
